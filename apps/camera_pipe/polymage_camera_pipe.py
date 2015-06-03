6c8a4dabaf20a3ca6aebe0ef7fe2f8e0d3d0e108
Vinay Vasista <vinay.vasista@csa.iisc.ernet.in>
2015-06-04 01:01:00 +0530

    campipe matrix reference

--- apps/camera_pipe/polymage_camera_pipe.py
+++ apps/camera_pipe/polymage_camera_pipe.py
@@ -270,10 +270,10 @@ def camera_pipe():
     # "Get a color matrix by linearly interpolating between two
     # calibrated matrices using inverse kelvin."
 
+    # reference values:
     # matrix_3200 = [[ 1.6697, -0.2693, -0.4004, -42.4346],
     #                [-0.3576,  1.0615,  1.5949, -37.1158],
     #                [-0.2175, -1.8751,  6.9640, -26.6970]]
-
     # matrix_7000 = [[ 2.2997, -0.4478,  0.1706, -39.0923],
     #                [-0.3826,  1.5906, -0.2080, -25.4311],
     #                [-0.0888, -0.7344,  2.2832, -20.0826]]
